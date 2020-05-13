ea7b0a797d86325a286afb9d0e1d479b8a9fdcb4
lukssds <lukssds@gmail.com>
2020-05-13 20:55:52 -0300

    Leitura da escolaridade

--- src/candidato/exerciciocandidato.java
+++ src/candidato/exerciciocandidato.java
@@ -6,7 +6,22 @@ import java.util.Scanner;
 public class exerciciocandidato {
 
 	public static void main(String[] args) {
-	
+		Locale.setDefault(Locale.US);		
+		Scanner sc= new Scanner(System.in);
+		
+		int opcoes,pontoes,pontoex,experiencia,pontototal;
+		char viagem;
+		char habilitacao;
+		
+		//leitura do escolaridade
+		
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.print("Digite uma opção: ");
+		
 		
 	}
 	
