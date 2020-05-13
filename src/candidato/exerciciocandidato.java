6b88f276ddca61a2f219be68f46495a32eaf301d
lukssds <lukssds@gmail.com>
2020-05-13 20:58:08 -0300

    Leitura dos outros dados

--- src/candidato/exerciciocandidato.java
+++ src/candidato/exerciciocandidato.java
@@ -21,6 +21,52 @@ public class exerciciocandidato {
 		System.out.println("3) Ensino Superior");
 		System.out.println("4) Pós-graduação");
 		System.out.print("Digite uma opção: ");
+		opcoes= sc.nextInt();
+
+		
+		
+		
+		if (opcoes==1) {
+			
+			pontoes=10;
+			
+		}
+		else if (opcoes==2) {
+			pontoes=20;
+		}
+		else if (opcoes==3) {
+			pontoes=30;
+			
+		}
+		else {
+			pontoes=40;
+		}
+		
+		System.out.println("");
+		System.out.print("Você tem quantos anos de experiência profissional? ");
+		experiencia=sc.nextInt();
+		System.out.print("Você tem disponibilidade para viajar (S/N)? ");
+		viagem=sc.next().charAt(0);
+		System.out.print("Você tem habilitação de motorista (S/N)? ");
+		habilitacao=sc.next().charAt(0);
+		
+		if (experiencia==0) {
+			pontoex=0;
+			System.out.println("Sem experiência");
+		}
+		
+		else if (experiencia<=2) {
+			pontoex=10;
+			
+		}
+		else if (experiencia<=5) {
+			pontoex=20;
+			
+		}
+		else   {
+			pontoex=40;
+			
+		}
 		
 		
 	}
